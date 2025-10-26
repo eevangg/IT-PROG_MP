3165e71072666a5e863d84776b26a25ec601da04
henryagunanne <henryagunanne@gmail.com>
2025-10-26 14:57:23 +0800

    DB

---
+++ canteen_preorder_db.sql
@@ -0,0 +1,183 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema canteen_preorder_db
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema canteen_preorder_db
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `canteen_preorder_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci ;
+USE `canteen_preorder_db` ;
+
+-- -----------------------------------------------------
+-- Table `canteen_preorder_db`.`menu_items`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `canteen_preorder_db`.`menu_items` (
+  `item_id` INT NOT NULL AUTO_INCREMENT,
+  `item_name` VARCHAR(100) NOT NULL,
+  `category` ENUM('Breakfast', 'Lunch', 'Snack') NULL DEFAULT NULL,
+  `price` DECIMAL(10,2) NOT NULL,
+  `description` TEXT NULL DEFAULT NULL,
+  `status` ENUM('active', 'inactive') NULL DEFAULT 'active',
+  PRIMARY KEY (`item_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci;
+
+
+-- -----------------------------------------------------
+-- Table `canteen_preorder_db`.`inventory_logs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `canteen_preorder_db`.`inventory_logs` (
+  `log_id` INT NOT NULL AUTO_INCREMENT,
+  `item_id` INT NOT NULL,
+  `change_type` ENUM('restock', 'order', 'adjustment') NOT NULL,
+  `quantity_changed` INT NOT NULL,
+  `log_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`log_id`),
+  INDEX `item_id` (`item_id` ASC),
+  CONSTRAINT `inventory_logs_ibfk_1`
+    FOREIGN KEY (`item_id`)
+    REFERENCES `canteen_preorder_db`.`menu_items` (`item_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci;
+
+
+-- -----------------------------------------------------
+-- Table `canteen_preorder_db`.`meal_plans`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `canteen_preorder_db`.`meal_plans` (
+  `plan_id` INT NOT NULL AUTO_INCREMENT,
+  `item_id` INT NOT NULL,
+  `day_of_week` ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday') NOT NULL,
+  `week_start` DATE NOT NULL,
+  `available_qty` INT NULL DEFAULT '0',
+  `status` ENUM('available', 'unavailable') NULL DEFAULT 'available',
+  PRIMARY KEY (`plan_id`),
+  INDEX `item_id` (`item_id` ASC),
+  CONSTRAINT `meal_plans_ibfk_1`
+    FOREIGN KEY (`item_id`)
+    REFERENCES `canteen_preorder_db`.`menu_items` (`item_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci;
+
+
+-- -----------------------------------------------------
+-- Table `canteen_preorder_db`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `canteen_preorder_db`.`users` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `full_name` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(100) NULL DEFAULT NULL,
+  `user_type` ENUM('student', 'staff', 'admin') NULL DEFAULT 'student',
+  `balance` DECIMAL(10,2) NULL DEFAULT '0.00',
+  `date_created` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` ENUM('active', 'inactive') NULL DEFAULT 'active',
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `username` (`username` ASC),
+  UNIQUE INDEX `email` (`email` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci;
+
+
+-- -----------------------------------------------------
+-- Table `canteen_preorder_db`.`orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `canteen_preorder_db`.`orders` (
+  `order_id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `plan_id` INT NULL DEFAULT NULL,
+  `total_amount` DECIMAL(10,2) NOT NULL,
+  `payment_method` ENUM('wallet', 'cash', 'qr', 'card') NULL DEFAULT 'wallet',
+  `order_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `pickup_time` TIME NULL DEFAULT NULL,
+  `status` ENUM('pending', 'confirmed', 'preparing', 'ready', 'completed', 'cancelled') NULL DEFAULT 'pending',
+  PRIMARY KEY (`order_id`),
+  INDEX `user_id` (`user_id` ASC),
+  INDEX `plan_id` (`plan_id` ASC),
+  CONSTRAINT `orders_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `canteen_preorder_db`.`users` (`user_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `orders_ibfk_2`
+    FOREIGN KEY (`plan_id`)
+    REFERENCES `canteen_preorder_db`.`meal_plans` (`plan_id`)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci;
+
+
+-- -----------------------------------------------------
+-- Table `canteen_preorder_db`.`order_details`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `canteen_preorder_db`.`order_details` (
+  `detail_id` INT NOT NULL AUTO_INCREMENT,
+  `order_id` INT NOT NULL,
+  `item_id` INT NOT NULL,
+  `quantity` INT NOT NULL,
+  `subtotal` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`detail_id`),
+  INDEX `order_id` (`order_id` ASC),
+  INDEX `item_id` (`item_id` ASC),
+  CONSTRAINT `order_details_ibfk_1`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `canteen_preorder_db`.`orders` (`order_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `order_details_ibfk_2`
+    FOREIGN KEY (`item_id`)
+    REFERENCES `canteen_preorder_db`.`menu_items` (`item_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci;
+
+
+-- -----------------------------------------------------
+-- Table `canteen_preorder_db`.`payments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `canteen_preorder_db`.`payments` (
+  `payment_id` INT NOT NULL AUTO_INCREMENT,
+  `order_id` INT NOT NULL,
+  `payment_method` ENUM('wallet', 'cash', 'qr', 'card') NULL DEFAULT 'wallet',
+  `amount_paid` DECIMAL(10,2) NOT NULL,
+  `payment_status` ENUM('pending', 'paid', 'failed', 'refunded') NULL DEFAULT 'pending',
+  `payment_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`payment_id`),
+  INDEX `order_id` (`order_id` ASC),
+  CONSTRAINT `payments_ibfk_1`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `canteen_preorder_db`.`orders` (`order_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
