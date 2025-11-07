5d9f05152cff0acbd6703f397ffc970e27082c86
henryagunanne <henryagunanne@gmail.com>
2025-11-07 09:14:53 +0800

    updates

---
+++ src/homepage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home - Order Management System</title>
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <header>
+            <nav class="navbar">
+                <div class="logo">
+                    <a href="homepage.php"><h1>Order Management System</h1></a>
+                </div>
+                
+                <ul>
+                    <li><a href="create.php">Create Order</a></li>
+                    <li><a href="read.php">View Orders</a></li>
+                    <li><a href="update.php">Edit Order</a></li>
+                    <li><a href="delete.php">Delete Orders</a></li>
+                </ul>
+            </nav>
+        </header>
+        
+        <main>
+            <div class="container">
+                <h2>Welcome to the Order Management System</h2>
+                <p>Use the navigation links above to manage your orders.</p>
+            </div>
+        </main>
+    </body>
+</html>
